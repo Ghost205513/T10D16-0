5e82f35a52b9011dd0b55ea176d4eee8f3ce8abb
lucioana <nikita.suslov.03@mail.ru>
2022-07-20 21:46:23 +0300

    quest1-6

---
+++ src/s21_string.h
@@ -0,0 +1,11 @@
+#ifndef SRC_S21_STRING_H_
+#define SRC_S21_STRING_H_
+
+int s21_strlen(char *str);
+int s21_strcmp(char *str1, char *str2);
+int s21_strcpy(char *dst, char *src);
+int s21_strcat(char *src, char *add);
+char *s21_strchr(char *src, int search);
+char *s21_strstr(char *src, char *search);
+
+#endif  // SRC_S21_STRING_H_
